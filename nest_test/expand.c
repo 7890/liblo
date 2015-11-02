9070c5e9b1fc7c0cc5b861614284c0a874eaed7b
Thomas Brand <tom@trellis.ch>
2015-11-02 01:36:32 +0100

    add /tb typed blob handler, test with float and int blobs

--- nest_test/expand.c
+++ nest_test/expand.c
@@ -11,18 +11,41 @@
 
 //also see nest.c
 
+/*
+lo_blobsize:
+A function to calculate the amount of OSC message space required by a lo_blob object.
+Returns the storage size in bytes, which will always be a multiple of four.
+
+//padded, inclduing length header
+uint32_t lo_blobsize(lo_blob b)
+{
+    const uint32_t len = sizeof(uint32_t) + b->size;
+    return 4 * ((len + 3) / 4);
+}
+
+//data body
+uint32_t lo_blob_datasize(lo_blob b)
+{
+    return b->size;
+}
+
+lo_blob_datasize < lo_blobsize
 */
 
-void print_blob(lo_blob b, int indent);
-void print_indent(int indent);
-void print_msg(lo_message msg, const char * path, int indent);
+static void print_blob(lo_blob b, int indent);
+static void print_indent(int indent);
+static void print_msg(lo_message msg, const char * path, int indent);
 
-void print_blob(lo_blob b, int indent)
+static int bflat_pos=0;
+static int vflat_pos=0;
+
+//=============================================================================
+void print_blob(const lo_blob b, int indent)
 {
 /*
 	unsigned char *q = lo_blob_dataptr(b);
 	int k;
-	for (k = 0; k < lo_blobsize(b)-4; k++) //-4 !
+	for (k = 0; k < lo_blob_datasize(b); k++)
 	{
 		fprintf(stderr,"%c",q[k]);
 	}
@@ -33,21 +56,27 @@ void print_blob(lo_blob b, int indent)
 
 	if(*path!='/')
 	{
+		fprintf(stderr,"b %d ",lo_blobsize(b));
 		fprintf(stderr,"(blob with unknown encoding)\n");
 	}
 	else
 	{
-		lo_message msg=lo_message_deserialise(lo_blob_dataptr(b),lo_blobsize(b)-4,NULL); //-4!
+//		fprintf(stderr,"b(M) (%d + %d bytes) ",lo_blobsize(b)-lo_blob_datasize(b),lo_blob_datasize(b));
+		fprintf(stderr,"b(M) ");
+
+		lo_message msg=lo_message_deserialise(lo_blob_dataptr(b),lo_blob_datasize(b),NULL);
 		indent++;
+		bflat_pos++;
 		//======
 		print_msg(msg,path,indent);
 		lo_message_free(msg);
 	}
 }
 
-void print_indent(int indent)
+//=============================================================================
+void print_indent(const int indent)
 {
-	int spaces=3;
+	const int spaces=3;
 	int i;
 	for(i=0;i<indent*spaces;i++)
 	{
@@ -55,42 +84,102 @@ void print_indent(int indent)
 	}
 }
 
-void print_msg(lo_message msg, const char * path, int indent)
+//=============================================================================
+void print_msg(const lo_message msg, const char * path, int indent)
 {
-	int arg_count=lo_message_get_argc(msg);
+	const int arg_count=lo_message_get_argc(msg);
 	const char *types=lo_message_get_types(msg);
 	lo_arg ** arg_values=lo_message_get_argv(msg);
 
-	fprintf(stderr,"%s %s\n",path,types);
+	fprintf(stderr,"%s %s (%lu bytes)\n",path,types,lo_message_length(msg,path));
+
+	if(!strcmp(path,"/tb") && !strcmp(types,"cb"))
+	{
+		const lo_blob b=(lo_blob)arg_values[1];
+		const uint32_t size = lo_blobsize(b);
+		const uint32_t dsize = lo_blob_datasize(b);
+
+		//handle typed blob
+		if(arg_values[0]->c=='f')
+		{
+			const float *q=lo_blob_dataptr(b);
+
+			const int num=dsize/sizeof(float);
+
+			print_indent(indent);
+			fprintf(stderr,"f %d {",num);
+
+			int k;
+			for (k = 0; k < num; k++)
+			{
+				fprintf(stderr,"%f",q[k]); //default
+//				fprintf(stderr,"%.2f",q[k]); //limit 
+				if(k<num-1)
+				{
+					fprintf(stderr," ");
+				}
+		
+			}
+			fprintf(stderr,"}\n");
+
+			vflat_pos++;
+			vflat_pos++;
+			return;
+		}
+		else if(arg_values[0]->c=='i')
+		{
+			///
+			const float *q=lo_blob_dataptr(b);
+
+			const int num=dsize/sizeof(int32_t);
+
+			print_indent(indent);
+			fprintf(stderr,"i %d {",num);
+			int k;
+			for (k = 0; k < num; k++)
+			{
+				fprintf(stderr,"%d",(int32_t)q[k]);
+				if(k<num-1)
+				{
+					fprintf(stderr," ");
+				}
+			}
+			fprintf(stderr,"}\n");
+
+			vflat_pos++;
+			vflat_pos++;
+			return;
+		}
+	}
 
 	int i;
 	for(i=0;i<arg_count;i++)
 	{
+		print_indent(indent);
 		if(types[i]=='b')
 		{
-			print_indent(indent);
-			fprintf(stderr,"b %d ",lo_blobsize((lo_blob)arg_values[i]));
+			//fprintf(stderr,"+ %d) ",i);
 			print_blob((lo_blob)arg_values[i],indent);
 		}
 		else if(types[i]=='f')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"f %f\n",arg_values[i]->f);
 		}
 		else if(types[i]=='i')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"i %d\n",arg_values[i]->i);
 
 		}
 		else if(types[i]=='s')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"s \"%s\"\n",&arg_values[i]->s);
 		}
 		else if(types[i]=='m')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"m ");
 			int w;
 			for(w=0;w<4;w++)
@@ -102,12 +191,12 @@ void print_msg(lo_message msg, const char * path, int indent)
 		}
 		else if(types[i]=='h')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"h %lu\n",arg_values[i]->h);
 		}
 		else if(types[i]=='t')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			lo_timetag tt=(lo_timetag)arg_values[i]->t;
 /*
 uint32_t lo_timetag::frac
@@ -120,55 +209,62 @@ The number of seconds since Jan 1st 1900 in the UTC timezone.
 		}
 		else if(types[i]=='d')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"d %f\n",arg_values[i]->d);
 		}
 		else if(types[i]=='S')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"S %d\n",arg_values[i]->S);
 		}
 		else if(types[i]=='c')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"c %c\n",arg_values[i]->c);
 		}
 		else if(types[i]=='T')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"T TRUE\n");
 		}
 		else if(types[i]=='F')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"F FALSE\n");
 		}
 		else if(types[i]=='N')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"N NIL\n");
 		}
 		else if(types[i]=='I')
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"I INFINITUM\n");
 		}
 		else
 		{
-			print_indent(indent);
+			//fprintf(stderr,"%d %d) ",vflat_pos,i);
 			fprintf(stderr,"%c UNKNOWN TYPE\n",types[i]);
+		}
+
+		///
+		if(types[i]!='b')
+		{
+			vflat_pos++;
 		}
 	}
 
 	return;
 }
 
+//=============================================================================
 int main(int argc, char *argv[])
 {
 	//http://stackoverflow.com/questions/14002954/c-programming-how-to-read-the-whole-file-contents-into-a-buffer
 	FILE *f = fopen(argv[1], "rb");
 	fseek(f, 0, SEEK_END);
-	long fsize = ftell(f);
+	const uint32_t fsize = ftell(f);
 	fseek(f, 0, SEEK_SET);
 	char *bytes = malloc(fsize);
 	fread(bytes, fsize, 1, f);
--- nest_test/nest.c
+++ nest_test/nest.c
@@ -8,42 +8,16 @@
 
 //gcc -o nest nest.c -llo && ./nest > /tmp/a && hexdump -c /tmp/a
 
-//trying to wrap lo_message_add
-lo_blob sub(const char *types, ... )
-{
-	lo_message msg=lo_message_new();
-	const char* path="/";
+lo_blob sub(const char *path, const char *types, ... );
 
-	//from src/message.c, int lo_message_add(lo_message msg, const char *types, ...)
-	va_list ap;
-	const char *file = "";
-	const int line = 0;
-	va_start(ap, types);
-
-	//return
-	lo_message_add_varargs_internal(msg, types, ap, file, line);
-	//va_end (ap);
-	//some problem here:liblo error: lo_send, lo_message_add, or lo_message_add_varargs called with mismatching types and data at:0, exiting.
-
-	void * msgbytes=calloc(lo_message_length(msg,path),sizeof(char));
-	size_t size_ret;
-	lo_message_serialise (msg, path, msgbytes, &size_ret);
-
-	lo_message_free(msg);
-	//free(msgbytes); ////!?
-
-	fprintf(stderr,"serialized %lu bytes\n",size_ret);
-	lo_blob b=lo_blob_new(size_ret,msgbytes);
-	fprintf(stderr,"blob size %d bytes\n",lo_blobsize(b));
-
-	//blob to be freed by caller
-	return b;
-}
-
+//=============================================================================
 int main(int argc, char *argv[])
 {
-	const float float_array[3] = {0.1,0.2,0.3};
+	const float float_array[12] = {-123.456789,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,100000};
 	lo_blob bfloat = lo_blob_new(sizeof(float_array), float_array);
+
+	const float int_array[12] = {-100000,0,1,2,3,4,5,6,7,8,9,100000};
+	lo_blob bint = lo_blob_new(sizeof(int_array), int_array);
 
 	uint8_t midi_data[4] = { 0xff, 0xf7, 0xAA, 0x00 };
 
@@ -53,14 +27,14 @@ int main(int argc, char *argv[])
 	lo_timetag timetag = { 0x1, 0x80000000 };
 	//lo_timetag timetag = { 0x1, 0x00000000 };
 
-	lo_blob b1=sub("sif","a string grouped with an int and a float",42,0.123);
+	lo_blob b1=sub("/b1","sif","a string grouped with an int and a float",42,0.123);
 	lo_blob b2=
-	sub("bbbifb"
+	sub("/b2","bbbifb"
 		,b1 //containing previously created msg blob
 		,bfloat //containing (non-msg) float array blob
-		,sub("sb"
+		,sub("/x","sb"
 			,"named float list"
-			,sub("fff"
+			,sub("/","fff" //
 				,0.1
 				,0.2
 				,0.3
@@ -68,7 +42,7 @@ int main(int argc, char *argv[])
 		)
 		,1
 		,2.3
-		,sub("hTdScmtbb" //param without arg in-between
+		,sub("/","hTdScmtbb" //param without arg in-between
 			,0x0123456789abcdefULL //int64
 			//(true)
 			,0.9999 //double
@@ -76,12 +50,21 @@ int main(int argc, char *argv[])
 			,'X' //char
 			,midi_data
 			,timetag
-			,sub("TFNIb"
+			,sub("/hi","TFNIbbb"
 				//(true)
 				//(false)
 				//(nil)
 				//(infinitum)
 				,b1
+				,sub("/tb","cb" //typed blob
+					,'f'
+					,bfloat
+				)
+				,sub("/tb","cb" //typed blob
+					,'i'
+					,bint
+				)
+
 			)
 			,b1
 		)
@@ -116,42 +99,76 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+//trying to wrap lo_message_add
+//=============================================================================
+lo_blob sub(const char *path, const char *types, ... )
+{
+	lo_message msg=lo_message_new();
+	//const char* path="/";
+
+	va_list ap;
+	va_start(ap, types); //last non-... arg
+
+	//from src/message.c, int lo_message_add(lo_message msg, const char *types, ...)
+	//return lo_message_add_varargs_internal(msg, types, ap, file, line);
+	lo_message_add_varargs(msg,types,ap);
+	//va_end (ap);
+	//some problem here:liblo error: lo_send, lo_message_add, or lo_message_add_varargs called with mismatching types and data at:0, exiting.
+
+	void * msgbytes=calloc(lo_message_length(msg,path),sizeof(char));
+	size_t size_ret;
+	lo_message_serialise (msg, path, msgbytes, &size_ret);
+
+	lo_message_free(msg);
+	//free(msgbytes); ////!?
+
+	fprintf(stderr,"serialized %lu bytes\n",size_ret);
+	lo_blob b=lo_blob_new(size_ret,msgbytes);
+	fprintf(stderr,"blob size %d bytes\n",lo_blobsize(b));
+
+	//blob to be freed by caller
+	return b;
+}
+
 /*
 expected output for this example:
 
 raw, 392 bytes
 (here base64 encoded)
 
-L3Rlc3QAAAAsaWZzYgAAAAAAAIA+TMzNZm9vAAAAAXQvAAAALGJiYmlmYgAAAABALwAAACxzaWYA
+L3Rlc3QAAAAsaWZzYgAAAAAAAIA+TMzNZm9vAAAAAiQvYjIALGJiYmlmYgAAAABAL2IxACxzaWYA
 AAAAYSBzdHJpbmcgZ3JvdXBlZCB3aXRoIGFuIGludCBhbmQgYSBmbG9hdAAAAAAAAAAqPfvnbQAA
-AAzNzMw9zcxMPpqZmT4AAAA4LwAAACxzYgBuYW1lZCBmbG9hdCBsaXN0AAAAAAAAABgvAAAALGZm
-ZgAAAAA9zMzNPkzMzT6ZmZoAAAABQBMzMwAAAMwvAAAALGhUZFNjbXRiYgAAASNFZ4mrze8/7/8u
-SOinHnN5bQAAAABY//eqAAAAAAGAAAAAAAAAUC8AAAAsVEZOSWIAAAAAAEAvAAAALHNpZgAAAABh
-IHN0cmluZyBncm91cGVkIHdpdGggYW4gaW50IGFuZCBhIGZsb2F0AAAAAAAAACo9++dtAAAAQC8A
-AAAsc2lmAAAAAGEgc3RyaW5nIGdyb3VwZWQgd2l0aCBhbiBpbnQgYW5kIGEgZmxvYXQAAAAAAAAA
-Kj37520=
+ADDg6fbCAAAAAM3MzD3NzEw+mpmZPs3MzD4AAAA/mpkZPzMzMz/NzEw/ZmZmPwBQw0cAAAA4L3gA
+ACxzYgBuYW1lZCBmbG9hdCBsaXN0AAAAAAAAABgvAAAALGZmZgAAAAA9zMzNPkzMzT6ZmZoAAAAB
+QBMzMwAAAVgvAAAALGhUZFNjbXRiYgAAASNFZ4mrze8/7/8uSOinHnN5bQAAAABY//eqAAAAAAGA
+AAAAAAAA3C9oaQAsVEZOSWJiYgAAAAAAAABAL2IxACxzaWYAAAAAYSBzdHJpbmcgZ3JvdXBlZCB3
+aXRoIGFuIGludCBhbmQgYSBmbG9hdAAAAAAAAAAqPfvnbQAAAEAvdGIALGNiAAAAAGYAAAAw4On2
+wgAAAADNzMw9zcxMPpqZmT7NzMw+AAAAP5qZGT8zMzM/zcxMP2ZmZj8AUMNHAAAAQC90YgAsY2IA
+AAAAaQAAADAAUMPHAAAAAAAAgD8AAABAAABAQAAAgEAAAKBAAADAQAAA4EAAAABBAAAQQQBQw0cA
+AABAL2IxACxzaWYAAAAAYSBzdHJpbmcgZ3JvdXBlZCB3aXRoIGFuIGludCBhbmQgYSBmbG9hdAAA
+AAAAAAAqPfvnbQ==
 
 expanded (see expand.c)
 
-/test ifsb
+/test ifsb (580 bytes)
 i 128
 f 0.200000
 s "foo"
-b 376 / bbbifb
-   b 68 / sif
+b(M) /b2 bbbifb (548 bytes)
+   b(M) /b1 sif (64 bytes)
       s "a string grouped with an int and a float"
       i 42
       f 0.123000
-   b 16 (blob with unknown encoding)
-   b 60 / sb
+   b 52 (blob with unknown encoding)
+   b(M) /x sb (56 bytes)
       s "named float list"
-      b 28 / fff
+      b(M) / fff (24 bytes)
          f 0.100000
          f 0.200000
          f 0.300000
    i 1
    f 2.300000
-   b 208 / hTdScmtbb
+   b(M) / hTdScmtbb (344 bytes)
       h 81985529216486895
       T TRUE
       d 0.999900
@@ -159,16 +176,20 @@ b 376 / bbbifb
       c X
       m 255 (0xff) 247 (0xf7) 170 (0xaa) 0 (0x00) 
       t 00000001.80000000
-      b 84 / TFNIb
+      b(M) /hi TFNIbbb (220 bytes)
          T TRUE
          F FALSE
          N NIL
          I INFINITUM
-         b 68 / sif
+         b(M) /b1 sif (64 bytes)
             s "a string grouped with an int and a float"
             i 42
             f 0.123000
-      b 68 / sif
+         b(M) /tb cb (64 bytes)
+            f 12 {-123.456787 0.000000 0.100000 0.200000 0.300000 0.400000 0.500000 0.600000 0.700000 0.800000 0.900000 100000.000000}
+         b(M) /tb cb (64 bytes)
+            i 12 {-100000 0 1 2 3 4 5 6 7 8 9 100000}
+      b(M) /b1 sif (64 bytes)
          s "a string grouped with an int and a float"
          i 42
          f 0.123000
